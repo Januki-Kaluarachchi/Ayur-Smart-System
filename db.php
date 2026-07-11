5d332ef7822c2d4f9524b8e6c34aadeec61a2e36
Januki-Kaluarachchi <madse009nibm@gmail.com>
2026-07-11 07:57:05 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,20 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "ayur_db";
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    $conn->set_charset("utf8mb4");
+
+} catch (mysqli_sql_exception $e) {
+
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
