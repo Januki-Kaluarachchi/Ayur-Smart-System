603216e09dd5f07926eeb54c118b1d049a36db8c
Januki-Kaluarachchi <madse009nibm@gmail.com>
2026-07-18 11:21:30 +0530

    Create doctor_register_process.php

---
+++ doctor_register_process.php
@@ -0,0 +1,29 @@
+<?php
+include 'db.php';
+header('Content-Type: application/json');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $doctor_name = $_POST['doctor_name'];
+    $specialization = $_POST['specialization'];
+    $schedule = $_POST['schedule']; 
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
+    $medical_license_id = $_POST['medical_license_id'];
+    $status = 'pending'; 
+
+    $stmt = $conn->prepare("INSERT INTO doctors (doctor_name, specialization, schedule, username, password, medical_license_id, status) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    
+ 
+    $stmt->bind_param("sssssss", $doctor_name, $specialization, $schedule, $username, $password, $medical_license_id, $status);
+
+    if ($stmt->execute()) {
+        echo json_encode(["status" => "success", "message" => "Registration successful! Pending Admin approval."]);
+    } else {
+        echo json_encode(["status" => "error", "message" => $stmt->error]);
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
